2a3bf66ab8d0f7425c679006e3c69025a80e6f03
florian czorniuk <florian.czorniuk@web.de>
2025-10-24 21:58:22 +0200

    Logo fix, mobile broken again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,18 +41,28 @@ export default function RootLayout({
           {/* <main className="flex flex-col items-center w-full min-h-screen p-4 bg-gradient-to-b from-[#2c1e68] via-[#1a4c9c] to-[#2aa4d8] sm:p-6 md:p-10">*/}
           <main className="flex flex-col items-center w-full min-h-screen p-4 bg-gradient-to-br from-background-light via-accent-light to-primary-50 dark:from-background-dark dark:via-primary-600 dark:to-primary-700 sm:p-6 md:p-10">
             {/* Header mit Datum und Logo */}
-            <div className="relative w-full flex flex-col sm:flex-row items-center justify-between mb-4 px-4 sm:px-0">
-              {/* Logo mittig auf Mobile, links auf Desktop */}
-              <div className="order-2 sm:order-1 sm:absolute sm:left-1/2 sm:-translate-x-1/2">
-                <Logo
-                  width={300}
-                  height={300}
-                  className="w-[200px] h-[200px] sm:w-[300px] sm:h-[300px]"
-                />
+            <div className="relative w-full mb-[-60px]">
+              {/* Logo - centered above content on mobile, absolute on desktop */}
+              <div className="flex justify-center sm:absolute sm:left-1/2 sm:-translate-x-1/2 sm:top-0">
+                <div className="flex items-center justify-center gap-4">
+                  <Logo
+                    width={300}
+                    height={300}
+                    className="w-[200px] h-[200px] sm:w-[300px] sm:h-[300px]"
+                  />
+                  <div
+                    className="w-px bg-gray-400 dark:bg-gray-600 self-center mt-[-180px]"
+                    style={{ height: "100px" }}
+                  ></div>
+                  <Logo
+                    width={300}
+                    height={300}
+                    className="w-[200px] h-[200px] sm:w-[300px] sm:h-[300px]"
+                  />
+                </div>
               </div>
               {/* Datum oben rechts */}
-              <div className="order-1 sm:order-2 sm:absolute sm:right-0">
-                {" "}
+              <div className="flex justify-end">
                 {/* TODO: Messtation Datum und Uhrzeit anzeigen*/}
                 <DateTime />
               </div>
